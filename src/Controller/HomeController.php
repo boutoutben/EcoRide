a7f0eb33ae2a67812231750417cbdd7cc3ff1c21
boutoutben <boutout.ben@gmail.com>
2024-11-18 21:36:52 +0100

    footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render("Home/home.html.twig");
