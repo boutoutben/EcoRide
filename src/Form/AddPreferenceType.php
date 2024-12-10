46fa03b7c2df8641c9f9c60e231a1863d9f04553
boutoutben <boutout.ben@gmail.com>
2024-12-10 09:40:42 +0100

    info conducteur de covoiturage

--- src/Form/AddPreferenceType.php
+++ src/Form/AddPreferenceType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class AddPreferenceType extends AbstractType
 {
@@ -15,6 +16,10 @@ class AddPreferenceType extends AbstractType
         $builder
             ->add('newPreference',TextType::class,[
                 "required" => false,
+            "constraints" => [
+                new Assert\NotBlank(message: "Le champ ne peut pas être vide"),
+                new Assert\Regex("/^[A-Z]{2}-\d{3}-[A-Z]{2}$/", "La préférence n'est pas conforme")
+            ],
                 "attr" => [
                     "class" => "input-place",
                     "placeholder" => "préference",
